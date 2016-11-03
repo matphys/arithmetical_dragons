f25b67d86956d52597b312183ec6a129a7555a76
azat <razilya5858@yandex.ru>
2016-11-03 16:21:48 +0400

    Update dragon.h

--- src/dragon.h
+++ src/dragon.h
@@ -13,7 +13,7 @@ public:
     static const int c_killScores = 100;
 
     Dragon(std::string color, int start_health, int _attackForce)
-        :Unit(start_health, _attackForce), my_color(color)
+            :Unit(start_health, _attackForce), my_color(color)
     {}
 
     std::string generateQuestion();
@@ -35,8 +35,8 @@ class GreenDragon: public Dragon
 public:
 
     GreenDragon()
-        :Dragon("Green", c_greenDragonHealth,
-                 c_greenDragonAttackForce)
+            :Dragon("Green", c_greenDragonHealth,
+                    c_greenDragonAttackForce)
     {}
     std::string generateQuestion()
     {
@@ -51,8 +51,50 @@ public:
 };
 
 //class RedDragon;
+class RedDragon: public Dragon
+{
+    static const int c_redDragonHealth = 50;
+    static const int c_redDragonAttackForce = 5;
+public:
+
+    RedDragon()
+            :Dragon("Red", c_redDragonHealth,
+                    c_redDragonAttackForce)
+    {}
+    std::string generateQuestion()
+    {
+        int left = 1 + rand()%100;
+        int right = 1 + rand()%100;
+
+        std::stringstream question;
+        question << left << " - " << right << ": ";
+        my_answer = left - right;
+        return question.str();
+    }
+};
 
 //class BlackDragon;
+class BlackDragon: public Dragon
+{
+    static const int c_blackDragonHealth = 50;
+    static const int c_blackDragonAttackForce = 5;
+public:
+
+    BlackDragon()
+            :Dragon("Black", c_blackDragonHealth,
+                    c_blackDragonAttackForce)
+    {}
+    std::string generateQuestion()
+    {
+        int left = 1 + rand()%100;
+        int right = 1 + rand()%100;
+
+        std::stringstream question;
+        question << left << " * " << right << ": ";
+        my_answer = left * right;
+        return question.str();
+    }
+};
 
 
 #endif // DRAGON_H_INCLUDED
